cc4725eacdf0da8f90181899c592edee5c9a59f4
Shreyas <shreyxs@gmail.com>
2021-05-20 23:44:38 +0530

    API url changed

--- src/backend.js
+++ src/backend.js
@@ -1,4 +1,4 @@
 // export const API = "https://dry-everglades-36635.herokuapp.com/api";
 // export const API = "http://192.168.0.186:5000/api";
-export const API = "https://ecom-back-6a7oc.ondigitalocean.app/api";
+export const API = "https://ecom-back-2prdl.ondigitalocean.app/api";
 // export const API = "http://192.168.43.143:8000/api";

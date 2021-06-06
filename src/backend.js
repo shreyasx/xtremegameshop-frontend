6104c29f19ebee193fd7bd6173b48062870de3ed
Shreyas Jamkhandi <65876403+shreyasx@users.noreply.github.com>
2021-06-07 01:45:54 +0530

    Update backend.js

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,3 @@
 // export const API = "http://192.168.0.186:5000/api";
-export const API = "https://ecom-back-2prdl.ondigitalocean.app/api";
+export const API = "https://shreyasx-ecom-back.zeet.app/api";
 // export const API = "http://192.168.43.143:8000/api";

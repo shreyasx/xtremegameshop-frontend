90254ef0249f0aa24ee4900cbf7fdf394bd7223b
Shreyas <shreyxs@gmail.com>
2021-06-10 01:37:35 +0530

    changed to heroku API

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,3 @@
-export const API = "https://shreyasx-ecom-back.zeet.app/api";
+export const API = "https://xtremegameshop-api.herokuapp.com/api";
 // export const API = "http://localhost:5000/api";
 // export const API = "http://192.168.0.186:5000/api";

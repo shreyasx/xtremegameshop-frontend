8726fb85bb35d2dae5f8bdc461e4aec43ffc911a
Shreyas <shreyxs@gmail.com>
2021-06-24 18:39:37 +0530

    changed backend url

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,4 @@
-export const API = "https://xtremegameshop-api.herokuapp.com/api";
+// export const API = "https://xtremegameshop-api.herokuapp.com/api";
+export const API = "https://xtremegameshop-api-2.herokuapp.com/api";
 // export const API = "http://localhost:5000/api";
 // export const API = "http://192.168.0.186:5000/api";

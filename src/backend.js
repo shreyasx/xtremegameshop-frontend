4454400f37beb814827ff2963c0a85f7615a8506
Shreyas Jamkhandi <shreyxs@gmail.com>
2023-04-19 11:41:23 +0530

    removed depr pkg

--- src/backend.js
+++ src/backend.js
@@ -1,4 +1,4 @@
-export const API = "https://xtremegameshop-api.herokuapp.com/api";
+// export const API = "https://xtremegameshop-api.herokuapp.com/api";
 // export const API = "https://xtremegameshop-api-2.herokuapp.com/api";
-// export const API = "http://localhost:5001/api";
+export const API = "http://localhost:5001/api";
 // export const API = "http://192.168.0.186:5000/api";
